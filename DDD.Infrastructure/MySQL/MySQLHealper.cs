28c6e9bb1d865bf17460706ce4708dcabb333a57
yo1030 <0vant1c@gmail.com>
2023-05-28 11:39:15 +0900

    AreaData の取得を Helper 関数で簡略化

--- DDD.Infrastructure/MySQL/MySQLHealper.cs
+++ DDD.Infrastructure/MySQL/MySQLHealper.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DDD.Domain.Entities;
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,5 +18,21 @@ namespace DDD.Infrastructure.MySQL
         internal readonly static string pass = "tEKjUXbdztd5@";
         internal readonly static int port = 3306;
         internal readonly static string connectionString = string.Format("Server={0};Database={1};User={2};Password={3};Port={4}", server, database, user, pass, port);
+
+        internal static IReadOnlyList<T> Query<T>(
+            string sql,
+            Func<MySqlDataReader, T> createEntity)
+        {
+            List<T> result = new List<T>();
+            using (MySqlCommand cmd = new MySqlCommand(sql, new MySqlConnection(MySQLHealper.connectionString)))
+            using (MySqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    result.Add(createEntity(reader));
+                }
+            }
+            return result;
+        }
     }
 }
