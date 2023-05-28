be3afa85105d69145cd756ad83d60af39771ed3b
yo1030 <0vant1c@gmail.com>
2023-05-28 12:30:16 +0900

    MySQL の戻り値が Entity のものを Helper 化
    Helper関数にコマンドパラメーターを追加

--- DDD.Infrastructure/MySQL/MySQLHealper.cs
+++ DDD.Infrastructure/MySQL/MySQLHealper.cs
@@ -23,16 +23,64 @@ namespace DDD.Infrastructure.MySQL
             string sql,
             Func<MySqlDataReader, T> createEntity)
         {
+            return Query<T>(sql, null, createEntity);
+        }
+        internal static IReadOnlyList<T> Query<T>(
+            string sql,
+            MySqlParameter[] parameters,
+            Func<MySqlDataReader, T> createEntity)
+        {
             List<T> result = new List<T>();
-            using (MySqlCommand cmd = new MySqlCommand(sql, new MySqlConnection(MySQLHealper.connectionString)))
-            using (MySqlDataReader reader = cmd.ExecuteReader())
+            using (MySqlConnection connection = new MySqlConnection(MySQLHealper.connectionString))
+            using (MySqlCommand cmd = new MySqlCommand(sql, connection))
             {
-                while (reader.Read())
+                connection.Open();
+                if (parameters != null)
                 {
-                    result.Add(createEntity(reader));
+                    cmd.Parameters.AddRange(parameters);
                 }
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        result.Add(createEntity(reader));
+                    }
+                }
+                return result;
             }
-            return result;
+        }
+
+        internal static T QuerySingle<T>(
+            string sql,
+            Func<MySqlDataReader, T> createEntity,
+            T nullEntity)
+        {
+            return QuerySingle<T>(sql, null, createEntity, nullEntity);
+        }
+
+            internal static T QuerySingle<T>(
+            string sql,
+            MySqlParameter[] parameters,
+            Func<MySqlDataReader, T> createEntity,
+            T nullEntity)
+        {
+            using (MySqlConnection connection = new MySqlConnection(MySQLHealper.connectionString))
+            using (MySqlCommand cmd = new MySqlCommand(sql, connection))
+            {
+                connection.Open();
+                if (parameters != null)
+                {
+                    cmd.Parameters.AddRange(parameters);
+                }
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        return createEntity(reader);
+                    }
+                }
+                return nullEntity;
+            }
         }
     }
 }
--- DDD.Infrastructure/MySQL/WeatherMySQL.cs
+++ DDD.Infrastructure/MySQL/WeatherMySQL.cs
@@ -2,6 +2,7 @@
 using DDD.Domain.Repositories;
 using MySql.Data.MySqlClient;
 using System;
+using System.Collections.Generic;
 
 namespace DDD.Infrastructure.MySQL
 {
@@ -13,16 +14,31 @@ namespace DDD.Infrastructure.MySQL
 Conditions,
 Temperature
 FROM weather
-WHERE AreaId={areaId}
+WHERE AreaId=@areaId
 ORDER BY DataDate DESC
 LIMIT 1;";
 
+            return MySQLHealper.QuerySingle(
+                sql,
+                new List<MySqlParameter>
+                {
+                    new MySqlParameter("areaId", areaId)
+                }.ToArray(),
+                reader =>
+                {
+                    return new WeatherEntity(
+                            areaId,
+                            Convert.ToDateTime(reader["DataDate"]),
+                            Convert.ToInt32(reader["Conditions"]),
+                            Convert.ToSingle(reader["Temperature"]));
+                },
+                null);
             using (MySqlConnection connection = new MySqlConnection(MySQLHealper.connectionString))
+            using (MySqlCommand cmd = new MySqlCommand(sql, connection))
             {
-                connection.Open();	// 接続
+                connection.Open();  // 接続
                 Console.WriteLine("MySQLに接続しました！");
-
-                using (MySqlCommand cmd = new MySqlCommand(sql, connection))
+                cmd.Parameters.Add(new MySqlParameter("areaId", areaId));
                 using (MySqlDataReader reader = cmd.ExecuteReader())
                 {
                     while (reader.Read())
