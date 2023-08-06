6d25fc322b750681c9375e39175d0e097434c529
yo1030 <0vant1c@gmail.com>
2023-08-06 15:39:32 +0900

    WPF作成
    ステータスバー
    ボタン作成

---
+++ DDD.WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace DDD.WPF.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "DDD";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
---
+++ DDD.WPF/Views/LatestView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DDD.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for LatestView
+    /// </summary>
+    public partial class LatestView : UserControl
+    {
+        public LatestView()
+        {
+            InitializeComponent();
+        }
+    }
+}
